9544bc6483592354ce03a87cacf4bb670a59875a
Andrea <roundcrisis@gmail.com>
2016-03-28 21:11:46 +0100

    adding base version of wander

---
+++ src/Treatz/Behaviours.fs
@@ -0,0 +1,33 @@
+﻿module Behaviours
+
+open System
+open TreatzGame
+
+type WanderBehaviour  = {
+  CircleRadius : double
+  CircleDistance : double
+  RateOfChangeOfDirection : double
+
+  WanderingAngle: double
+  LastDirectionChangeTime : int  
+  SteeringDirection : Point
+  }
+
+type Vehicle = {
+  Velocity: Point
+  Position: Point
+}
+
+let wander gameTimeSeconds (random: Random) vehicle state  =     
+    let timeDiff = gameTimeSeconds = state.LastDirectionChangeTime
+    let currentWanderAngle = state.WanderingAngle + state.RateOfChangeOfDirection * (random.NextDouble() * 2.0 - 1.0)   
+
+    let circlePosition = vehicle.Position + (state.CircleDistance * vehicle.Velocity.normalize   )
+    let circleOffset = {
+      X = state.CircleRadius * Math.Cos(state.WanderingAngle)
+      Y = state.CircleRadius * Math.Sin(state.WanderingAngle) 
+      }
+    let steeringDirection = ( circlePosition + circleOffset)  - vehicle.Position
+            
+    { state with LastDirectionChangeTime = gameTimeSeconds; SteeringDirection = steeringDirection ; WanderingAngle = currentWanderAngle}
+
