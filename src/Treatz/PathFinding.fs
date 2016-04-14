d7159b873f3cab012416b12dc644cb782ca858fb
Andrea <roundcrisis@gmail.com>
2016-04-14 23:39:41 +0100

    improvement on the path finding

--- src/Treatz/PathFinding.fs
+++ src/Treatz/PathFinding.fs
@@ -14,13 +14,7 @@
         if Map.containsKey (i.X, i.Y) allNodes then
             yield allNodes.[(i.X, i.Y)] |]
 
-  let createInitialFrontier state : HashSet<Node> =
-    let f = HashSet<Node>()
-    f.Add(state) |> ignore
-    f
-
-  // not the most efficient way to calc distance but ...
-  let calcDistance (node:Node) goalNode =    
+  let costDistance (node:Node) goalNode =    
     abs (node.Identity.X - goalNode.Identity.X) + abs( node.Identity.Y - goalNode.Identity.Y) 
 
   let convertToArray (pathNode :NodePath) =
@@ -29,34 +23,54 @@
       | None -> array
       | Some parent -> someFn parent (Array.append [|n.GridNode|] array)    
     someFn pathNode Array.empty 
+
+  let areNeighbours n1 n2 = 
+    let difX = abs(n1.X - n2.X) = 1 
+    let difY = abs(n1.Y - n2.Y) = 1
+
+    match difX, difY with
+    | true, true | false, false -> false
+    | true, _ | _, true -> true
+
+  let findNeigbourParent nodePath node =
+    let rec findP nodePath =
+      match nodePath.Parent with
+      | None ->   nodePath
+      | Some p -> if (areNeighbours p.GridNode.Identity node.Identity) then 
+                      p 
+                  else
+                      findP p
+    findP nodePath
     
-  
-  let search startNode goal : Node array =
-    let frontier = createInitialFrontier startNode 
-    let explored = new HashSet<Node>() //mutable    
+
+  let search startNode goal : Node array =    
+    let frontier = HashSet<Node>() 
+    frontier.Add startNode |> ignore 
+    let explored = new HashSet<Node>() 
     let mutable currentPathNode = {Parent = None; PathCost= 0.; GridNode = startNode}    
     
     if (frontier.Count = 0) then 
         [||]
     else  
-      while frontier.Count > 0 do      
-        let currentNode = 
-            frontier.ToArray() 
-            |> Seq.minBy(fun x -> calcDistance x goal + x.Cost + int currentPathNode.PathCost )
-        currentPathNode <- {currentPathNode with 
-                              Parent = Some(currentPathNode) 
-                              PathCost = double currentNode.Cost + currentPathNode.PathCost 
-                              GridNode= currentNode }
-        frontier.Remove(currentNode)  |> ignore
-        if (currentNode.Identity <> goal.Identity) then
-            explored.Add(currentNode) |> ignore 
+    while frontier.Count > 0 do      
+      let currentNode = 
+          frontier.ToArray() 
+          |> Seq.minBy(fun x -> costDistance x goal + x.Cost + int currentPathNode.PathCost )
+      currentPathNode <- { 
+                            Parent = if areNeighbours currentPathNode.GridNode.Identity currentNode.Identity then  
+                                        Some(currentPathNode)  
+                                     else Some(findNeigbourParent currentPathNode currentNode)
+                            PathCost = double currentNode.Cost + currentPathNode.PathCost 
+                            GridNode= currentNode }
+      frontier.Remove(currentNode)  |> ignore
+      if (currentNode.Identity <> goal.Identity) then
+          explored.Add(currentNode) |> ignore 
           
-            (currentNode.Neighbours)
-            |> Seq.iter(fun n -> 
-                  if not(explored.Contains(n)) then 
-                    frontier.Add(n) |> ignore )          
-        else
-            frontier.Clear()  //yuck          
-      convertToArray currentPathNode
-
+          (currentNode.Neighbours)
+          |> Seq.iter(fun n -> 
+                if not(explored.Contains(n)) then 
+                  frontier.Add(n) |> ignore )          
+      else
+          frontier.Clear()  //yuck          
+    convertToArray currentPathNode
 
